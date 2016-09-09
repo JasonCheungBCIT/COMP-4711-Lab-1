f19f08c4542980e756e740e7253039cfd6086b8a
Jason Cheung <jayson.h.cheung@gmail.com>
2016-09-08 17:31:04 -0700

    Added Student class and changed index.php to use student objects.

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,7 +7,45 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Hello World ... my name is Jason";
+        include ('Student.php'); // Includes the Student.php file found at root. 
+
+        $students = array();     // Create an array to hold all the students.
+
+        // Create students and add to array 
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+        
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+
+        $third = new Student();
+        $third->surname = "Cheung";
+        $third->first_name = "Jason";
+        $third->add_email('home', 'jason@cheung.com');
+        $third->add_grade(100);
+        $students['j999'] = $third;
+
+        ksort($students);    // Sort the students by key 
+
+        // Output formatted student info to webpage 
+        foreach($students as $student)
+            echo $student->toString();
+
         ?>
     </body>
 </html>
